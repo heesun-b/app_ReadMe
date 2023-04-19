fe96687495efc06588f4752bfffa262decd4308e
unknown <yuhyunk7@gmail.com>
2023-04-19 16:50:54 +0900

    first complice

--- lib/view/page/main/main_page/main_page.dart
+++ lib/view/page/main/main_page/main_page.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:readme_app/core/constants/colours.dart';
+import 'package:readme_app/core/constants/dimens.dart';
 import 'package:readme_app/core/constants/yh_style_icons.dart';
 import 'package:card_swiper/card_swiper.dart';
 
@@ -18,37 +20,147 @@ final List<String> imgList = [
   "assets/images/book3.png",
 ];
 
+
+int _selectedButtonIndex = 0;
+
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Container(
-        height: 450,
-        width: double.infinity,
-        child: Swiper(
-          controller: SwiperController(),
-          pagination: SwiperPagination(),
-          itemCount: imgList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Stack(
-              children: [
-                ImageFiltered(
-                  imageFilter: ImageFilter.blur(sigmaX: 40, sigmaY: 10),
-                  child: Image.asset(imgList[index], fit: BoxFit.cover),
-                ),
-                Container(
-                  height: 450,
-                  width: 500,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Image.asset(imgList[index]),
-                  ),
-                ),
-              ],
-            );
-          },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildAdScreen(),
+            _buildMainListBar(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Container _buildMainListBar() {
+    return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                          fontSize: Dimens.font_sp20,
+                          fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedButtonIndex = 0;
+                      });
+                    },
+                    child: Text(
+                      "전체",
+                      style: TextStyle(
+                        color: _selectedButtonIndex == 0 ? Colours.app_main : Colours.app_sub_black,
+                        fontSize: Dimens.font_sp20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                          fontSize: Dimens.font_sp20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedButtonIndex = 1;
+                      });
+                    },
+                    child: Text(
+                      "베스트셀러",
+                      style: TextStyle(
+                        color: _selectedButtonIndex == 1 ? Colours.app_main : Colours.app_sub_black,
+                        fontSize: Dimens.font_sp20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                          fontSize: Dimens.font_sp20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedButtonIndex = 2;
+                      });
+                    },
+                    child: Text(
+                      "추천",
+                      style: TextStyle(
+                        color: _selectedButtonIndex == 2 ? Colours.app_main : Colours.app_sub_black,
+                        fontSize: Dimens.font_sp20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                          fontSize: Dimens.font_sp20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedButtonIndex = 3;
+                      });
+                    },
+                    child: Text(
+                      "신간",
+                      style: TextStyle(
+                        color: _selectedButtonIndex == 3 ? Colours.app_main : Colours.app_sub_black,
+                        fontSize: Dimens.font_sp20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+  }
+
+  Container _buildAdScreen() {
+    return Container(
+      height: 450,
+      width: double.infinity,
+      child: Swiper(
+        controller: SwiperController(),
+        pagination: SwiperPagination(),
+        itemCount: imgList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Stack(
+            children: [
+              ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 40, sigmaY: 10),
+                child: Image.asset(imgList[index], fit: BoxFit.cover),
+              ),
+              Container(
+                height: 450,
+                width: 500,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Image.asset(imgList[index]),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
