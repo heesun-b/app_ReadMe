158bc0de018bd207d064395f1ff7a0cfa336a2e5
unknown <yuhyunk7@gmail.com>
2023-04-19 19:06:41 +0900

    main complite

--- lib/view/page/main/main_page/main_page.dart
+++ lib/view/page/main/main_page/main_page.dart
@@ -4,8 +4,12 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:readme_app/core/constants/colours.dart';
 import 'package:readme_app/core/constants/dimens.dart';
-import 'package:readme_app/core/constants/yh_style_icons.dart';
+import 'package:readme_app/core/constants/hs_style_icons.dart';
+
+import 'package:readme_app/core/constants/use_icons.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:readme_app/core/constants/yh_style_icons.dart';
+import 'package:readme_app/model/cart_mock_data.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -20,7 +24,6 @@ final List<String> imgList = [
   "assets/images/book3.png",
 ];
 
-
 int _selectedButtonIndex = 0;
 
 class _MainPageState extends State<MainPage> {
@@ -33,6 +36,87 @@ class _MainPageState extends State<MainPage> {
           children: [
             _buildAdScreen(),
             _buildMainListBar(),
+            Container(
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: cartList.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colours.app_sub_darkgrey),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/${cartList[index].image}",
+                            width: 100,
+                            height: 150,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${cartList[index].title}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 20,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 100,
+                              ),
+                              Text(
+                                  "${cartList[index].author} | ${cartList[index].store}",
+                                style: TextStyle(
+                                  fontSize: 16
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  YhIcons.star,
+                                  Text("${cartList[index].score}",
+                                  style: TextStyle(
+                                    fontSize: 16
+                                  ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("소장가 ${cartList[index].price}"),
+                                  SizedBox(width: 100),
+                                  IconButton(
+                                    padding: EdgeInsets.zero,
+                                    constraints: BoxConstraints(),
+                                    onPressed: () {},
+                                    icon: YhIcons.heart,
+                                  ),
+                                  IconButton(
+                                    padding: EdgeInsets.zero,
+                                    constraints: BoxConstraints(),
+                                    onPressed: () {},
+                                    icon: YhIcons.cart2,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -41,98 +125,108 @@ class _MainPageState extends State<MainPage> {
 
   Container _buildMainListBar() {
     return Container(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                          fontSize: Dimens.font_sp20,
-                          fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _selectedButtonIndex = 0;
-                      });
-                    },
-                    child: Text(
-                      "전체",
-                      style: TextStyle(
-                        color: _selectedButtonIndex == 0 ? Colours.app_main : Colours.app_sub_black,
-                        fontSize: Dimens.font_sp20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                          fontSize: Dimens.font_sp20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _selectedButtonIndex = 1;
-                      });
-                    },
-                    child: Text(
-                      "베스트셀러",
-                      style: TextStyle(
-                        color: _selectedButtonIndex == 1 ? Colours.app_main : Colours.app_sub_black,
-                        fontSize: Dimens.font_sp20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                          fontSize: Dimens.font_sp20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _selectedButtonIndex = 2;
-                      });
-                    },
-                    child: Text(
-                      "추천",
-                      style: TextStyle(
-                        color: _selectedButtonIndex == 2 ? Colours.app_main : Colours.app_sub_black,
-                        fontSize: Dimens.font_sp20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                          fontSize: Dimens.font_sp20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _selectedButtonIndex = 3;
-                      });
-                    },
-                    child: Text(
-                      "신간",
-                      style: TextStyle(
-                        color: _selectedButtonIndex == 3 ? Colours.app_main : Colours.app_sub_black,
-                        fontSize: Dimens.font_sp20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colours.app_sub_darkgrey),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: Dimens.font_sp20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedButtonIndex = 0;
+                });
+              },
+              child: Text(
+                "전체",
+                style: TextStyle(
+                  color: _selectedButtonIndex == 0
+                      ? Colours.app_main
+                      : Colours.app_sub_black,
+                  fontSize: Dimens.font_sp20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          );
+            Spacer(),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                    fontSize: Dimens.font_sp20, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedButtonIndex = 1;
+                });
+              },
+              child: Text(
+                "베스트셀러",
+                style: TextStyle(
+                  color: _selectedButtonIndex == 1
+                      ? Colours.app_main
+                      : Colours.app_sub_black,
+                  fontSize: Dimens.font_sp20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Spacer(),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                    fontSize: Dimens.font_sp20, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedButtonIndex = 2;
+                });
+              },
+              child: Text(
+                "추천",
+                style: TextStyle(
+                  color: _selectedButtonIndex == 2
+                      ? Colours.app_main
+                      : Colours.app_sub_black,
+                  fontSize: Dimens.font_sp20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Spacer(),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                    fontSize: Dimens.font_sp20, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedButtonIndex = 3;
+                });
+              },
+              child: Text(
+                "신간",
+                style: TextStyle(
+                  color: _selectedButtonIndex == 3
+                      ? Colours.app_main
+                      : Colours.app_sub_black,
+                  fontSize: Dimens.font_sp20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Container _buildAdScreen() {
@@ -177,13 +271,13 @@ AppBar _buildAppBar() {
     ),
     actions: [
       IconButton(
-        icon: UseIcons.alarm,
+        icon: YhIcons.alarm,
         onPressed: () {
           //Move.AlarmPage();
         },
       ),
       IconButton(
-          icon: UseIcons.cart,
+          icon: YhIcons.cart,
           onPressed: () {
             //Move.CartPage();
           })
