bb82d8f36854a7667de83cefb89cf7efb579ceb0
unknown <yuhyunk7@gmail.com>
2023-04-25 17:52:10 +0900

    소ã장 tabview add

--- lib/core/constants/move.dart
+++ lib/core/constants/move.dart
@@ -4,10 +4,11 @@ import 'package:readme_app/view/components/home_navigation_bar.dart';
 import 'package:readme_app/view/page/alarm/alarm_page/alarm_page.dart';
 import 'package:readme_app/view/page/book_detail/book_detail_page/book_detail_page.dart';
 import 'package:readme_app/view/page/book_detail/book_detail_page/components/modal_button_sheet.dart';
-import 'package:readme_app/view/page/book_detail/book_detail_page/components/modal_button_sheet_test.dart';
+
 import 'package:readme_app/view/page/category/category_page/category_page.dart';
 import 'package:readme_app/view/page/cart/cart_page/cart_page.dart';
 
+import 'package:readme_app/view/page/category/category_page/category_page.dart';
 import 'package:readme_app/view/page/main/main_page/main_page.dart';
 import 'package:readme_app/view/page/membership/membership_page/memebership_page.dart';
 import 'package:readme_app/view/page/membership_cancel/membership_cancel_page.dart';
@@ -17,6 +18,8 @@ import 'package:readme_app/view/page/question_list/question_list_page.dart';
 import 'package:readme_app/view/page/review/review_page.dart';
 import 'package:readme_app/view/page/payment_list/payment_list_page.dart';
 import 'package:readme_app/view/page/search/search_list_page/search_list_page.dart';
+import 'package:readme_app/view/page/payment/payment_page.dart';
+import 'package:readme_app/view/page/search/search_page/search_page.dart';
 import 'package:readme_app/view/page/storage/storage_page/storage_page.dart';
 import 'package:readme_app/view/page/user/user_page/user_page.dart';
 
--- lib/view/page/book_detail/book_detail_page/components/modal_button_sheet.dart
+++ lib/view/page/book_detail/book_detail_page/components/modal_button_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:readme_app/core/constants/yh_style_icons.dart';
+import 'package:readme_app/core/constants/colours.dart';
+import 'package:readme_app/model/book_detail_mock_data.dart';
 
 class ModalButtonSheet extends StatefulWidget {
   const ModalButtonSheet({Key? key}) : super(key: key);
@@ -77,7 +78,6 @@ class _ModalButtonSheetState extends State<ModalButtonSheet>
     );
   }
 
-
   void _showModalBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -89,7 +89,9 @@ class _ModalButtonSheetState extends State<ModalButtonSheet>
               child: Column(
                 children: [
                   TabBar(
+                    labelStyle: TextStyle(fontSize: 22),
                     controller: _tabController,
+                    indicatorColor: Colours.app_main,
                     tabs: [
                       Tab(text: '멤버십 구독'),
                       Tab(text: '소장'),
@@ -101,11 +103,103 @@ class _ModalButtonSheetState extends State<ModalButtonSheet>
                       children: [
                         Container(
                           color: Colors.white,
-                          child: Center(child: Text('Content of Tab 1')),
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'ReadMe 멤버가 아니신가요?',
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  '멤버십을 구독하고 모든 도서를 자유롭게 열람해 보세요.',
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '월 9,900원',
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '(VAT 포함)',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 10),
+                                MembershipButton(
+                                  text: '멤버십 구독하기',
+                                  onPressed: () => Navigator.pop(context),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                         Container(
                           color: Colors.white,
-                          child: Center(child: Text('Content of Tab 2')),
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  child: Image.asset(
+                                    "assets/images/book.jpg",
+                                    // height: 150,
+                                    // width: 150,
+                                  ),
+                                ),
+                                SizedBox(width: 30),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${bookDetail.title}',
+                                      style: TextStyle(
+                                          fontSize: 26,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      '${bookDetail.author}',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          '소장가 ${bookDetail.price}원',
+                                          style: TextStyle(fontSize: 18),
+                                        ),
+                                        Text(
+                                          '(VAT 포함)',
+                                          style: TextStyle(fontSize: 13),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        PurchaseButton(
+                          text: '소장하기',
+                          onPressed: () => Navigator.pop(context),
                         ),
                       ],
                     ),
@@ -116,6 +210,58 @@ class _ModalButtonSheetState extends State<ModalButtonSheet>
           },
         );
       },
+    );
+  }
+}
+
+class PurchaseButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const PurchaseButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(double.infinity, 50),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        backgroundColor: Colours.app_sub_black,
+        foregroundColor: Colours.app_sub_white,
+        textStyle: const TextStyle(fontSize: 18),
+      ),
+      child: Text(text),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class MembershipButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const MembershipButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(double.infinity, 50),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        backgroundColor: Colours.app_sub_black,
+        foregroundColor: Colours.app_sub_white,
+        textStyle: const TextStyle(fontSize: 18),
+      ),
+      child: Text(text),
+      onPressed: onPressed,
     );
   }
 }
