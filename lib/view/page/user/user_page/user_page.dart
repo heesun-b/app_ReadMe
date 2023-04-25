d6d1d62d25bc0ec8e7ce8cef9ea6dd43d367b41d
heesun <heesun9793@naver.com>
2023-04-25 09:57:45 +0900

    complete

--- lib/view/page/user/user_page/user_page.dart
+++ lib/view/page/user/user_page/user_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:readme_app/core/constants/colours.dart';
 import 'package:readme_app/core/constants/dimens.dart';
+import 'package:readme_app/core/constants/hs_style_icons.dart';
 import 'package:readme_app/core/constants/use_icons.dart';
 import 'package:readme_app/view/components/use_button.dart';
 
@@ -36,34 +37,42 @@ class _UserPageState extends State<UserPage> {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: UseIcons.questionMark,
-                iconSize: 25,
-              ),
-              Text("이용약관")
-            ],
+          padding: const EdgeInsets.all(20.0),
+          child: InkWell(
+            onTap: () {
+              // 추가
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                UseIcons.questionMark,
+                SizedBox(
+                  width: 5,
+                ),
+                Text("이용약관")
+              ],
+            ),
           ),
         ),
         Divider(
           thickness: 2,
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: UseIcons.infoMark,
-                iconSize: 25,
-              ),
-              Text("앱 정보")
-            ],
+          padding: const EdgeInsets.all(15.0),
+          child: InkWell(
+            onTap: () {
+              // 추가
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                UseIcons.infoMark,
+                SizedBox(
+                  width: 5,
+                ),
+                Text("앱 정보")
+              ],
+            ),
           ),
         )
       ],
@@ -82,16 +91,20 @@ class _UserPageState extends State<UserPage> {
                     bottom: BorderSide(color: Colours.app_sub_grey, width: 2.0),
                     right:
                         BorderSide(color: Colours.app_sub_grey, width: 2.0))),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: UseIcons.question,
-                  iconSize: 30,
-                ),
-                Text("문의하기")
-              ],
+            child: InkWell(
+              onTap: () {
+                // 추가
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  UseIcons.question,
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text("문의하기")
+                ],
+              ),
             ),
           ),
         ),
@@ -103,16 +116,20 @@ class _UserPageState extends State<UserPage> {
                 border: Border(
                     bottom:
                         BorderSide(color: Colours.app_sub_grey, width: 2.0))),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: UseIcons.questionList,
-                  iconSize: 30,
-                ),
-                Text("문의내역")
-              ],
+            child: InkWell(
+              onTap: () {
+                // 추가
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  UseIcons.questionList,
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text("문의내역")
+                ],
+              ),
             ),
           ),
         )
@@ -136,16 +153,20 @@ class _UserPageState extends State<UserPage> {
                           BorderSide(color: Colours.app_sub_grey, width: 2.0),
                       right:
                           BorderSide(color: Colours.app_sub_grey, width: 2.0))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: UseIcons.bookPayment,
-                    iconSize: 35,
-                  ),
-                  Text("구매도서")
-                ],
+              child: InkWell(
+                onTap: (){
+                  // 추가
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   HsStyleIcons.bookPayment,
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("구매도서")
+                  ],
+                ),
               ),
             ),
           ),
@@ -160,16 +181,20 @@ class _UserPageState extends State<UserPage> {
                           BorderSide(color: Colours.app_sub_grey, width: 2.0),
                       right:
                           BorderSide(color: Colours.app_sub_grey, width: 2.0))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: UseIcons.card,
-                    iconSize: 35,
-                  ),
-                  Text("결제내역")
-                ],
+              child: InkWell(
+                onTap: (){
+                  // 추가
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    HsStyleIcons.card,
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("결제내역")
+                  ],
+                ),
               ),
             ),
           ),
@@ -182,16 +207,20 @@ class _UserPageState extends State<UserPage> {
                       top: BorderSide(color: Colours.app_sub_grey, width: 2.0),
                       bottom:
                           BorderSide(color: Colours.app_sub_grey, width: 2.0))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: UseIcons.review,
-                    iconSize: 35,
-                  ),
-                  Text("리뷰관리")
-                ],
+              child: InkWell(
+                onTap: (){
+                  // 추가
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  HsStyleIcons.review,
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("리뷰관리")
+                  ],
+                ),
               ),
             ),
           )
@@ -239,12 +268,11 @@ class _UserPageState extends State<UserPage> {
     return Padding(
       padding: const EdgeInsets.all(15),
       child: Container(
-          height: 200,
           width: double.infinity,
           decoration: BoxDecoration(
               color: Colours.app_main, borderRadius: BorderRadius.circular(10)),
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
                 Row(
@@ -285,30 +313,39 @@ class _UserPageState extends State<UserPage> {
                     ),
                     IconButton(
                       onPressed: () {
-                        showDialog(context: context, builder: (context) {
-                        return AlertDialog(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          scrollable: false,
-                          title: Text("스탠다드 구독권이란?"),
-                          content: SizedBox(
-                            height: 90,
-                            child: Column(
-                              children: [
-                                Text("월 9,900원으로 ReadMe의 전체 도서를 열람할 수 있는 정기 구독권입니다."),
-                                SizedBox(height: 10,),
-                                Text("최초 결제일 기준으로 매달 자동 결제가 되어 편리하게 사용 가능합니다."),
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              scrollable: false,
+                              title: Text("스탠다드 구독권이란?"),
+                              content: SizedBox(
+                                height: 90,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                        "월 9,900원으로 ReadMe의 전체 도서를 열람할 수 있는 정기 구독권입니다."),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                        "최초 결제일 기준으로 매달 자동 결제가 되어 편리하게 사용 가능합니다."),
+                                  ],
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context, '확인');
+                                    },
+                                    child: Text("확인"))
                               ],
-                            ),
-                          ),
-                          actions: [
-                            TextButton(onPressed: (){
-                              Navigator.pop(context, '확인');
-                            }, child: Text("확인"))
-                          ],
+                            );
+                          },
                         );
-                        },);
                       },
                       icon: UseIcons.questionMark,
                       iconSize: 15,
@@ -357,19 +394,22 @@ class _UserPageState extends State<UserPage> {
                         style: TextStyle(fontSize: Dimens.font_sp16)),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, right: 8),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                        child: Text("해지"),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colours.app_sub_black,
-                          textStyle: TextStyle(fontWeight: FontWeight.w700),),
+                SizedBox(
+                  height: 5,
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/membershipCancel");
+                      },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Text("해지"),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colours.app_sub_black,
+                      textStyle: TextStyle(fontWeight: FontWeight.w700),
                     ),
                   ),
                 )
