3127140702bce8ad2c6c45e65ba2545b748175fd
unknown <yuhyunk7@gmail.com>
2023-04-25 12:04:21 +0900

    멤버십 구독 modal button 완ì성

--- lib/view/page/book_detail/book_detail_page/components/modal_button_sheet.dart
+++ lib/view/page/book_detail/book_detail_page/components/modal_button_sheet.dart
@@ -37,39 +37,9 @@ class MyStatelessWidget extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(10),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const Text(
-                        'ReadMe 멤버가 아니신가요?',
-                        style: TextStyle(
-                          fontSize: 28,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      const Text(
-                        '멤버십을 구독하고 모든 도서를 자유롭게 열람해 보세요.',
-                        style: TextStyle(fontSize: 23),
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          const Text(
-                            '월 9,900원',
-                            style: TextStyle(fontSize: 23),
-                          ),
-                          const Text(
-                            '(VAT 포함)',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      MembershipButton(
-                        text: '멤버십 구독하기',
-                        onPressed: () => Navigator.pop(context),
-                      ),
+                    children: [
+                      TabBar(),
+                      _buildMembershipSection(context),
                     ],
                   ),
                 ),
@@ -81,6 +51,124 @@ class MyStatelessWidget extends StatelessWidget {
     );
   }
 }
+
+class TabBar extends StatefulWidget {
+  const TabBar({Key? key}) : super(key: key);
+
+  @override
+  _TabBarState createState() => _TabBarState();
+}
+
+class _TabBarState extends State<TabBar> {
+  int _selectedButtonIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedButtonIndex = 0;
+                });
+              },
+              child: Text(
+                "멤버십 구독",
+                style: TextStyle(
+                  color: _selectedButtonIndex == 0 ? Colors.black : Colors.grey,
+                  fontSize: 22,
+                  fontWeight: _selectedButtonIndex == 0
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                ),
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedButtonIndex = 1;
+                });
+              },
+              child: Text(
+                "소장",
+                style: TextStyle(
+                  color: _selectedButtonIndex == 1 ? Colors.black : Colors.grey,
+                  fontSize: 22,
+                  fontWeight: _selectedButtonIndex == 1
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 1,
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+Widget _buildMembershipSection(context) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      const Text(
+        'ReadMe 멤버가 아니신가요?',
+        style: TextStyle(
+          fontSize: 22,
+        ),
+      ),
+      SizedBox(height: 10),
+      const Text(
+        '멤버십을 구독하고 모든 도서를 자유롭게 열람해 보세요.',
+        style: TextStyle(fontSize: 22),
+      ),
+      SizedBox(height: 10),
+      Row(
+        children: [
+          const Text(
+            '월 9,900원',
+            style: TextStyle(fontSize: 22),
+          ),
+          const Text(
+            '(VAT 포함)',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+      SizedBox(height: 10),
+      MembershipButton(
+        text: '멤버십 구독하기',
+        onPressed: () => Navigator.pop(context),
+      ),
+    ],
+  );
+}
+
+
 
 class MembershipButton extends StatelessWidget {
   final String text;
