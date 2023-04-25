0b8b20bb69806569742d73bc36d5387129aa6cfc
aj124578 <aj124578@naver.com>
2023-04-25 11:07:10 +0900

    book viwer v2 - add epub view complete

--- lib/view/page/book_viwer/components/book_drawer.dart
+++ lib/view/page/book_viwer/components/book_drawer.dart
@@ -1,203 +1,212 @@
 import 'package:flutter/material.dart';
 import 'package:readme_app/core/constants/colours.dart';
 import 'package:readme_app/core/constants/dimens.dart';
-import 'package:readme_app/core/constants/jh_style_button_add_minus.dart';
+import 'package:readme_app/view/page/book_viwer/components/jh_style_button_add_minus.dart';
 
 import '../../../../core/constants/jh_style_icons.dart';
 
-class BookDrawer extends StatelessWidget {
+class BookDrawer extends StatefulWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
-  const BookDrawer({Key? key, required this.scaffoldKey}) : super(key: key);
+  BookDrawer({Key? key, required this.scaffoldKey}) : super(key: key);
+
+
+  @override
+  State<BookDrawer> createState() => _BookDrawerState();
+}
+
+class _BookDrawerState extends State<BookDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 40.0),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            ListTile(
-              trailing: Container(
-                width: 250,
-                child: Text(
-                  "배경 설정",
-                  style: TextStyle(
-                      fontSize: Dimens.font_sp20, fontWeight: FontWeight.w700),
+    return Container(
+      child: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 40.0),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              ListTile(
+                trailing: Container(
+                  width: 250,
+                  child: Text(
+                    "배경 설정",
+                    style: TextStyle(
+                        fontSize: Dimens.font_sp20, fontWeight: FontWeight.w700),
+                  ),
                 ),
+                contentPadding: EdgeInsets.only(right: 25),
               ),
-              contentPadding: EdgeInsets.only(right: 25),
-            ),
-            ListTile(
-              trailing: Container(
-                width: 250,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      child: IconButton(
-                          onPressed: () {
-                            print("검은색 테마");
-                          },
-                          icon: JHicons.colorCircleBlack),
-                    ),
-                    Container(
-                      child: IconButton(
-                          onPressed: () {
-                            print("하얀색 테마");
-                          },
-                          icon: JHicons.colorCircleWhite),
-                    ),
-                    Container(
-                      child: IconButton(
-                          onPressed: () {
-                            print("메인 테마");
-                          },
-                          icon: JHicons.colorCircleMain),
-                    ),
-                    Container(
-                      child: IconButton(
-                          onPressed: () {
-                            print("회색 테마");
-                          },
-                          icon: JHicons.colorCircleGrey),
-                    ),
-                  ],
+              ListTile(
+                trailing: Container(
+                  width: 250,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        child: IconButton(
+                            onPressed: () {
+                              print("검은색 테마");
+                            },
+                            icon: JHicons.colorCircleBlack),
+                      ),
+                      Container(
+                        child: IconButton(
+                            onPressed: () {
+                              print("하얀색 테마");
+                            },
+                            icon: JHicons.colorCircleWhite),
+                      ),
+                      Container(
+                        child: IconButton(
+                            onPressed: () {
+                              print("메인 테마");
+                            },
+                            icon: JHicons.colorCircleMain),
+                      ),
+                      Container(
+                        child: IconButton(
+                            onPressed: () {
+                              print("회색 테마");
+                            },
+                            icon: JHicons.colorCircleGrey),
+                      ),
+                    ],
+                  ),
                 ),
+                contentPadding: EdgeInsets.only(right: 30),
               ),
-              contentPadding: EdgeInsets.only(right: 30),
-            ),
-            ListTile(
-              trailing: Container(
-                width: 250,
-                child: Text(
-                  "글자 크기",
-                  style: TextStyle(
-                      fontSize: Dimens.font_sp20, fontWeight: FontWeight.w700),
+              ListTile(
+                trailing: Container(
+                  width: 250,
+                  child: Text(
+                    "글자 크기",
+                    style: TextStyle(
+                        fontSize: Dimens.font_sp20, fontWeight: FontWeight.w700),
+                  ),
                 ),
+                contentPadding: EdgeInsets.only(right: 25),
               ),
-              contentPadding: EdgeInsets.only(right: 25),
-            ),
-            ListTile(
-              trailing: Container(
-                height: 50,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Stack(
-                      children: [
-                        Container(
-                          width: 80,
-                          child: jhUseButtonAddMinus(
-                            title: "",
-                            buttonPressed: () {},
+              ListTile(
+                trailing: Container(
+                  height: 50,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            width: 80,
+                            child: jhUseButtonAddMinus(
+                              title: "",
+                              buttonPressed: () {},
+                            ),
                           ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 3),
-                          width: 80,
-                          child: Positioned(
-                            child: JHicons.minus,
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 3),
+                            width: 80,
+                            child: Positioned(
+                              child: JHicons.minus,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Stack(
-                      children: [
-                        Container(
-                          width: 80,
-                          child: jhUseButtonAddMinus(
-                            title: "",
-                            buttonPressed: () {},
+                        ],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            width: 80,
+                            child: jhUseButtonAddMinus(
+                              title: "",
+                              buttonPressed: () {},
+                            ),
                           ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 3),
-                          width: 80,
-                          child: Positioned(
-                            child: JHicons.add,
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 3),
+                            width: 80,
+                            child: Positioned(
+                              child: JHicons.add,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
+                        ],
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
                 ),
+                contentPadding: EdgeInsets.only(right: 50),
               ),
-              contentPadding: EdgeInsets.only(right: 50),
-            ),
-            SizedBox(
-              width: 5,
-            ),
-            ListTile(
-              trailing: Container(
-                width: 250,
-                child: Text(
-                  "폰트 설정",
-                  style: TextStyle(
-                      fontSize: Dimens.font_sp20, fontWeight: FontWeight.w700),
+              SizedBox(
+                width: 5,
+              ),
+              ListTile(
+                trailing: Container(
+                  width: 250,
+                  child: Text(
+                    "폰트 설정",
+                    style: TextStyle(
+                        fontSize: Dimens.font_sp20, fontWeight: FontWeight.w700),
+                  ),
                 ),
+                contentPadding: EdgeInsets.only(right: 25),
               ),
-              contentPadding: EdgeInsets.only(right: 25),
-            ),
-            ListTile(
-              trailing: Container(
-                height: 50,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Container(
-                      width: 80,
-                      child: TextButton(
-                        child: Text("나눔고딕",
+              ListTile(
+                trailing: Container(
+                  height: 50,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                        width: 80,
+                        child: TextButton(
+                          child: Text("나눔고딕",
+                              style: TextStyle(
+                                  color: Colours.app_sub_black,
+                                  fontWeight: FontWeight.w500)),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Container(
+                        width: 80,
+                        child: TextButton(
+                          child: Text("나눔명조",
+                              style: TextStyle(
+                                  color: Colours.app_sub_black,
+                                  fontWeight: FontWeight.w500)),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Container(
+                        width: 80,
+                        child: TextButton(
+                          child: Text(
+                            "마루부리",
                             style: TextStyle(
                                 color: Colours.app_sub_black,
-                                fontWeight: FontWeight.w500)),
-                        onPressed: () {},
-                      ),
-                    ),
-                    Container(
-                      width: 80,
-                      child: TextButton(
-                        child: Text("나눔명조",
-                            style: TextStyle(
-                                color: Colours.app_sub_black,
-                                fontWeight: FontWeight.w500)),
-                        onPressed: () {},
-                      ),
-                    ),
-                    Container(
-                      width: 80,
-                      child: TextButton(
-                        child: Text(
-                          "마루부리",
-                          style: TextStyle(
-                              color: Colours.app_sub_black,
-                              fontWeight: FontWeight.w500),
+                                fontWeight: FontWeight.w500),
+                          ),
+                          onPressed: () {},
                         ),
-                        onPressed: () {},
                       ),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                  ],
+                      SizedBox(
+                        width: 5,
+                      ),
+                    ],
+                  ),
                 ),
+                contentPadding: EdgeInsets.only(right: 30),
               ),
-              contentPadding: EdgeInsets.only(right: 30),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
