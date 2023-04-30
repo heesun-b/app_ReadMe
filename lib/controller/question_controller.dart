62e0a2b8aba82eeb2eeda325ef6c594f67ce716f
heesun <heesun9793@naver.com>
2023-04-30 17:12:51 +0900

    작업중

---
+++ lib/controller/question_controller.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:readme_app/main.dart';
+
+final questionControllerProvider = Provider<QuestionController>((ref) {
+  return QuestionController(ref);
+});
+
+class QuestionController {
+  final mContext = navigatorKey.currentContext;
+  final Ref ref;
+
+  QuestionController(this.ref);
+
+
+  // 한 건 조회
+
+
+}
--- lib/view/page/question_list/components/question_list_page_body.dart
+++ lib/view/page/question_list/components/question_list_page_body.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:readme_app/core/constants/colours.dart';
 import 'package:readme_app/view/page/question_list/components/question_list_detail.dart';
+import 'package:readme_app/view/page/question_list/question_list_page_view_model.dart';
 
-class QuestionListPageBody extends StatelessWidget {
+class QuestionListPageBody extends ConsumerWidget {
   const QuestionListPageBody({Key? key}) : super(key: key);
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    QuestionListPageModel? model = ref.watch(questionListPageProvider);
+    List<Question> question = [];
+    if (model != null) {
+      question = model.questions;
+    }
+
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -31,10 +40,10 @@ class QuestionListPageBody extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text("2023.04.01", style: TextStyle(
+                          Text(model!.questions[index].time, style: TextStyle(
                               fontWeight: FontWeight.w700
                           )),
-                          Text("진행중", style: TextStyle(
+                          Text(model!.questions[index].status, style: TextStyle(
                               color: Colours.app_sub_blue
                           ),),
                         ],
